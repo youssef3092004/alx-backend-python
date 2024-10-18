4bc805decd6825cb67acdfe07cddbfe945eb84e8
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-18 16:12:58 +0300

    Implement wait_n async routine to gather delays in ascending order without using sort()

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 import asyncio
-wait_random = __import__("0-basic_async_syntax").wait_random
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n, max_delay):
-    delays = []
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    for task in asyncio.as_completed(tasks):
-        delay = await task
-        insert_pos = 0
-        while insert_pos < len(delays) and delays[insert_pos] < delay:
-            insert_pos += 1
-        delays.insert(insert_pos, delay)
-    return delays
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    delays = [wait_random(max_delay) for i in range(n)]
+    result = [await delay for delay in asyncio.as_completed(delays)]
+    return result
